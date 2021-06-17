5b8955048daa0584e0e91a9c89a258238f3684c8
server <curtis.x.jenkins@gmail.com>
2021-06-17 18:16:25 -0400

    making landing page for my fork

--- launch_openpilot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-export PASSIVE="0"
-export SKIP_FW_QUERY=1 
-exec ./launch_chffrplus.sh
-
--- update_requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-cd "$DIR"
-sudo -- bash -c "source /etc/profile.d/comma_dev.sh; pip install pip==20.1.1 pipenv==2020.8.13; pipenv install --dev --deploy --system"
