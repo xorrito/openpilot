a02b022fdc906f158c678ae2659ba6ceb04b8215
dkiiv <curtis.x.jenkins@gmail.com>
2024-04-11 10:30:47 -0400

    golf 6 kombi haxs (ACC equipped) (#53)
    * golf varient haxs (ACC equipped) (#50)
    
    * golf varient haxs (ACC equipped) (#49)
    
    * golf varient haxs (ACC equipped)
    
    * re-enable UDS FW query on ign
    
    * remove min steer speed, oops
    
    * omit accFault check until radar is fixed
    
    * omit ret.minEnableSpeed for PASSAT_NMS

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -37,10 +37,7 @@ class CarInterface(CarInterfaceBase):
       else:
         ret.transmissionType = TransmissionType.manual
 
-      if any(msg in fingerprint[1] for msg in (0x1A0, 0xC2)):  # Bremse_1, Lenkwinkel_1
-        ret.networkLocation = NetworkLocation.gateway
-      else:
-        ret.networkLocation = NetworkLocation.fwdCamera
+      ret.networkLocation = NetworkLocation.gateway
 
       # The PQ port is in dashcam-only mode due to a fixed six-minute maximum timer on HCA steering. An unsupported
       # EPS flash update to work around this timer, and enable steering down to zero, is available from:
@@ -48,7 +45,7 @@ class CarInterface(CarInterfaceBase):
       # It is documented in a four-part blog series:
       #   https://blog.willemmelching.nl/carhacking/2022/01/02/vw-part1/
       # Panda ALLOW_DEBUG firmware required.
-      ret.dashcamOnly = True
+      ret.dashcamOnly = False
 
     else:
       # Set global MQB parameters
@@ -128,9 +125,8 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.PASSAT_NMS:
       ret.mass = 1503
-      ret.wheelbase = 2.80
-      ret.minEnableSpeed = 20 * CV.KPH_TO_MS  # ACC "basic", no FtS
-      ret.minSteerSpeed = 50 * CV.KPH_TO_MS
+      ret.wheelbase = 2.62
+      #ret.minEnableSpeed = 20 * CV.KPH_TO_MS  # ACC "basic", no FtS
       ret.steerActuatorDelay = 0.2
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
