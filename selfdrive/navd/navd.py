5ae60f54a6678e2bc91ac5427a15f731dd2ac2b1
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-09 22:23:57 -0700

    FrogPilot features - Navigate on openpilot without a comma prime subscription
    Added functionality to use navigate on openpilot without a comma prime subscription by using a custom MapBox key.

--- selfdrive/navd/navd.py
+++ selfdrive/navd/navd.py
@@ -57,6 +57,9 @@ class RouteEngine:
     if "MAPBOX_TOKEN" in os.environ:
       self.mapbox_token = os.environ["MAPBOX_TOKEN"]
       self.mapbox_host = "https://api.mapbox.com"
+    elif self.params.get_int("PrimeType") == 0:
+      self.mapbox_token = self.params.get("MapboxPublicKey", encoding='utf8')
+      self.mapbox_host = "https://api.mapbox.com"
     else:
       self.api = Api(self.params.get("DongleId", encoding='utf8'))
       self.mapbox_host = "https://maps.comma.ai"
