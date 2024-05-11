8d1ce7a2d8e1534abb21ceb1f244073492490413
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-11 15:45:59 -0700

    Visuals - Screen Management - Screen Brightness
    Customize your screen brightness when offroad.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -303,6 +303,7 @@ typedef struct UIScene {
   int map_style;
   int obstacle_distance;
   int obstacle_distance_stock;
+  int screen_brightness;
   int steering_angle_deg;
   int stopped_equivalence;
   int wheel_icon;
