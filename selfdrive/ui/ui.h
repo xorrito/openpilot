c6319a4aa6db9e6362000ae9b04ec14e341e74a3
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-11 15:58:46 -0700

    Visuals - Screen Management - Standby Mode
    Turn the screen off after your screen times out when onroad, but wake it back up when engagement state changes or important alerts are triggered.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -252,6 +252,7 @@ typedef struct UIScene {
   bool speed_limit_changed;
   bool speed_limit_controller;
   bool speed_limit_overridden;
+  bool standby_mode;
   bool standstill;
   bool static_pedals_on_ui;
   bool tethering_enabled;
@@ -264,6 +265,7 @@ typedef struct UIScene {
   bool use_si;
   bool use_vienna_slc_sign;
   bool vtsc_controlling_curve;
+  bool wake_up_screen;
   bool wheel_speed;
 
   float acceleration;
