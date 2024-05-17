421d6eb7e9fea91da58a2ac055968c527cc131dc
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-05-17 11:35:31 -0400

    remove unused import from carstate

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -5,7 +5,6 @@ from openpilot.selfdrive.car.interfaces import CarStateBase
 from opendbc.can.parser import CANParser
 from openpilot.selfdrive.car.volkswagen.values import DBC, CANBUS, PQ_CARS, NetworkLocation, TransmissionType, GearShifter, \
                                             CarControllerParams, VolkswagenFlags
-from openpilot.selfdrive.car.volkswagen.carcontroller import CarController as CC
 
 
 class CarState(CarStateBase):
