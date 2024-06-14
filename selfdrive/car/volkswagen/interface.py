091b7f5a809dca92de693a1a8922533dbeb54fcf
Aaron Logan <86490693+FLcruising@users.noreply.github.com>
2024-06-14 07:41:17 -0500

    Update pid.kiV
    0.38

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -140,7 +140,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP = [0.0, 41.0]
       ret.lateralTuning.pid.kf = 0.00003
       ret.lateralTuning.pid.kpV = [0.13, 0.13]
-      ret.lateralTuning.pid.kiV = [0.42, 0.42]
+      ret.lateralTuning.pid.kiV = [0.38, 0.38]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.POLO_MK6:
