19b63e76a1b0ef65e8e65a902863ffac8dc50544
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-06-28 18:15:51 -0400

    update create_acc_accel_control for PQ, add TODO

--- selfdrive/car/volkswagen/pqcan.py
+++ selfdrive/car/volkswagen/pqcan.py
@@ -1,3 +1,5 @@
+from openpilot.common.numpy_fast import clip
+
 def create_steering_control(packer, bus, apply_steer, lkas_enabled):
   values = {
     "LM_Offset": abs(apply_steer),
@@ -47,7 +49,7 @@ def create_acc_buttons_control(packer, bus, gra_stock_values, cancel=False, resu
 
   return packer.make_can_msg("GRA_Neu", bus, values)
 
-
+# TODO: Add overrun status, where long is engaged but being overridden by driver
 def acc_control_value(main_switch_on, acc_faulted, long_active):
   if long_active:
     acc_control = 1
@@ -58,7 +60,7 @@ def acc_control_value(main_switch_on, acc_faulted, long_active):
 
   return acc_control
 
-
+# TODO: Add overrun status, where long is engaged but being overridden by driver
 def acc_hud_status_value(main_switch_on, acc_faulted, long_active):
   if acc_faulted:
     hud_status = 6
@@ -82,8 +84,8 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
     "ACS_Anhaltewunsch": acc_type == 1 and stopping,
     "ACS_FreigSollB": acc_enabled,
     "ACS_Sollbeschl": accel if acc_enabled else 3.01,
-    "ACS_zul_Regelabw": 0.2 if acc_enabled else 1.27,
-    "ACS_max_AendGrad": 3.0 if acc_enabled else 5.08,
+    "ACS_zul_Regelabw": 0.0 if acc_enabled else 1.27,
+    "ACS_max_AendGrad": clip(((accel * -1.5) if accel < 0 else 0), 0.5, 3.0) if acc_enabled else 5.08,
   }
 
   commands.append(packer.make_can_msg("ACC_System", bus, values))
