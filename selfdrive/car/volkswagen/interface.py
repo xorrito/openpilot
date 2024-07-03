aa7b27dea9390d2f6598e43da88442a6b20e4724
dkiiv <curtis.x.jenkins@gmail.com>
2024-07-03 12:16:34 -0400

    Update interface.py

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -79,7 +79,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kiBP = [0.0]
         ret.lateralTuning.pid.kf = 0.0000005
         ret.lateralTuning.pid.kpV = [0.010]
-        ret.lateralTuning.pid.kiV = [0.010]
+        ret.lateralTuning.pid.kiV = [0.008]
         ret.longitudinalTuning.kpV = [0.75]
         ret.longitudinalTuning.kiV = [0.50]
         CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
