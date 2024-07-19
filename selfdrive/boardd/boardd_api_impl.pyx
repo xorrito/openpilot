34dc1d86a55b925e166add4a9ecb47f7d205c871
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-07-18 20:50:41 -0400

    add debug print

--- selfdrive/boardd/boardd_api_impl.pyx
+++ selfdrive/boardd/boardd_api_impl.pyx
@@ -20,6 +20,7 @@ def can_list_to_can_capnp(can_msgs, msgtype='can', valid=True):
 
   cdef can_frame f
   for can_msg in can_msgs:
+    print(can_msg[0])
     f.address = can_msg[0]
     f.busTime = can_msg[1]
     f.dat = can_msg[2]
