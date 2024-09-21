9de51f98274aa2038a921c00559ee3790cd277b8
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-09-20 21:42:48 -0400

    VW PQ: limit OP long carAPI velocity deviation

--- selfdrive/car/volkswagen/pqcan.py
+++ selfdrive/car/volkswagen/pqcan.py
@@ -91,7 +91,7 @@ def create_acc_accel_control(packer, bus, acc_type, accel, acc_control, stopping
     "ACS_Anhaltewunsch": acc_type == 1 and stopping,
     "ACS_FreigSollB": acc_enabled,
     "ACS_Sollbeschl": accel if acc_enabled else 3.01,
-    "ACS_zul_Regelabw": 0.2 if acc_enabled else 1.27,
+    "ACS_zul_Regelabw": 0.05 if acc_enabled else 1.27,
     "ACS_max_AendGrad": 3.0 if acc_enabled else 5.08,
   }
 
