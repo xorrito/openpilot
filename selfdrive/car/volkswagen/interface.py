538c8c815902ecfd5613b67749c5ca8ebdf4f87d
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-09-28 19:00:38 -0400

    VW PQ: undo PI edit, need to do the long manuever test

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -73,7 +73,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.11
       ret.longitudinalTuning.kf = 1.
       ret.longitudinalTuning.kpBP = [0.]
-      ret.longitudinalTuning.kpV = [0.1]
+      ret.longitudinalTuning.kpV = [0.]
       ret.longitudinalTuning.kiBP = [0.]
       ret.longitudinalTuning.kiV = [0.]
       ret.longitudinalActuatorDelay = 0.15
