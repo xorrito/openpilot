559dcd32e08c641088a6c12fd220af86aea67c22
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-10-22 17:15:23 -0400

    VW PQ: update long-PIF

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -75,7 +75,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpBP = [0.]
       ret.longitudinalTuning.kpV = [0.15]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [0.02]
+      ret.longitudinalTuning.kiV = [0.03]
       ret.longitudinalActuatorDelay = 0.6
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
