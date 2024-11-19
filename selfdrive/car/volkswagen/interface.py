394040462ba17d36cba375ae4d1075efcc703d6b
dkiiv <curtis.x.jenkins@gmail.com>
2024-11-19 00:03:44 -0500

    Update interface.py

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -75,7 +75,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpBP = [0.]
       ret.longitudinalTuning.kpV = [0.]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [1.3]
+      ret.longitudinalTuning.kiV = [1.]
       ret.longitudinalActuatorDelay = 0.6
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
