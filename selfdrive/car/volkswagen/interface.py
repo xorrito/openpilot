ea0e32a1dae682698457604184ce27b9445ceb58
dkiiv <curtis.x.jenkins@gmail.com>
2024-11-20 13:00:17 -0500

    VW PQ: nerf kiV

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -75,7 +75,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpBP = [0.]
       ret.longitudinalTuning.kpV = [0.2]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [0.9]
+      ret.longitudinalTuning.kiV = [0.1]
       ret.longitudinalActuatorDelay = 0.6
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
