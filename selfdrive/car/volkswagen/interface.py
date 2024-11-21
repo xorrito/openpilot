9754837a56f14cac6e23f8a7f87d40ac9bd041b7
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-11-21 18:49:15 -0500

    VW PQ: adjusting kpv, kiv, kf

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -71,11 +71,11 @@ class CarInterface(CarInterfaceBase):
     ret.steerLimitTimer = 0.4
     if ret.flags & VolkswagenFlags.PQ:
       ret.steerActuatorDelay = 0.11
-      ret.longitudinalTuning.kf = 1.3
+      ret.longitudinalTuning.kf = 1.2
       ret.longitudinalTuning.kpBP = [0.]
-      ret.longitudinalTuning.kpV = [0.3]
+      ret.longitudinalTuning.kpV = [0.4]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [0.1]
+      ret.longitudinalTuning.kiV = [0.2]
       ret.longitudinalActuatorDelay = 0.6
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
