4d9ea910efbba6f8869811e3bf0b253125524aa4
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-12-06 15:14:11 -0500

    VW PQ: fix variable init

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -65,6 +65,7 @@ class CarController(CarControllerBase):
     self.EPB_brake_last = 0
     self.EPB_enable = 0
     self.EPB_enable_last = 0
+    self.EPB_enabled = 0
     self.EPB_counter = 0
     self.accel_diff = 0
     self.long_deviation = 0
@@ -281,14 +282,14 @@ class CarController(CarControllerBase):
 
       if CS.acc_sys_stock["COUNTER"] != self.acc_sys_counter_last:
         EPB_handler(CS, self, CS.acc_sys_stock["ACS_Sta_ADR"], CS.acc_sys_stock["ACS_Sollbeschl"], CS.out.vEgoRaw, self.stopped)
-        EPB_enabled = int((self.EPB_enable_last and not self.EPB_enable) or self.EPB_enable)
-        can_sends.append(self.CCS.filter_ACC_System(self.packer_pt, CANBUS.pt, CS.acc_sys_stock, EPB_enabled))
+        self.EPB_enabled = int((self.EPB_enable_last and not self.EPB_enable) or self.EPB_enable)
+        can_sends.append(self.CCS.filter_ACC_System(self.packer_pt, CANBUS.pt, CS.acc_sys_stock, self.EPB_enabled))
         can_sends.append(self.CCS.create_epb_control(self.packer_pt, CANBUS.br, self.EPB_brake, self.EPB_enable))
         can_sends.append(self.CCS.filter_epb1(self.packer_pt, CANBUS.cam, self.stopped))  # in custom module, filter the gateway fwd EPB msg
       if self.motor2_frame % 2 or CS.motor2_stock != getattr(self, 'motor2_last', CS.motor2_stock):  # 50hz / 20ms
-        can_sends.append(self.CCS.filter_motor2(self.packer_pt, CANBUS.cam, CS.motor2_stock, EPB_enabled))
+        can_sends.append(self.CCS.filter_motor2(self.packer_pt, CANBUS.cam, CS.motor2_stock, self.EPB_enabled))
       if CS.bremse8_stock["COUNTER"] != self.bremse8_counter_last:
-        can_sends.append(self.CCS.filter_bremse8(self.packer_pt, CANBUS.cam, CS.bremse8_stock, EPB_enabled))
+        can_sends.append(self.CCS.filter_bremse8(self.packer_pt, CANBUS.cam, CS.bremse8_stock, self.EPB_enabled))
       if CS.bremse11_stock["COUNTER"] != self.bremse11_counter_last:
         can_sends.append(self.CCS.filter_bremse11(self.packer_pt, CANBUS.cam, CS.bremse11_stock, self.stopped))
 
